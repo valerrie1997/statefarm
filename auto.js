b735ddcfcb2a0c4a0374086996bbd54d2cb421b8
valerrie1997 <30605107+valerrie1997@users.noreply.github.com>
2018-10-13 03:24:32 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 function extractDomain(url) {
 	var hostname;
-	if (url.indexOf("https://") > -1) {hostname = url.split('/')[2];}
+	if (url.indexOf("http://") > -1) {hostname = url.split('/')[2];}
 	else {hostname = url.split('/')[0];}
 	hostname = hostname.split(':')[0];
 	hostname = hostname.split('?')[0];
